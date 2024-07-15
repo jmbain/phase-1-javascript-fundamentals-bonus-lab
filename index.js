86ecd11befb013e37829d680eea74d99862dc946
Josh Bain <joshuambain@gmail.com>
2024-07-15 14:55:36 -0400

    added solution code for javascript fundamentals

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here
+// Instruction #1 solution
+const username = "alice123"
+// Instruction #2 solution
+const password = "flatironschool"
+// Instruction #3 solution... reminders 
+// let allows you to declare a variable without defining it and undefined is effectively falsey
+let isAuthorized 
+if(username === "alice123" && password === "flatironschool") {
+    isAuthorized = true
+}
+else {isAuthorized = false}
+
+// Instruction #4 solution
+const loginValidationPhrase = isAuthorized === true ? "Welcome " + username + "!" : "Invalid username or password!"
+
+// Instruction #5 solution
+const randomNumber = Number.parseInt(Math.random()*10) + 1
+//Incremental solution... const randomNumber = math.floor(Math.random()*10) +1
+
+// Instruction #6 solution
+const luckyNumberPhrase = `Your lucky number is ${randomNumber}!`
